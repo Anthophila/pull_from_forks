095b062ae9bf8673e5d96a48edbfbbf0e81ec150
ocramoirag <66082527+ocramoirag@users.noreply.github.com>
2020-05-29 10:37:42 +0200

    Revert "Main2.c"

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 main:
-	gcc main2.c -o main # New main
+	gcc main.c -o main
--- main2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, char **argv) {
-        char *copy2;
-        copy2 = (char *)malloc(strlen(argv[0]));
-        strcpy(copy2, argv[0]);
-        return copy2;
-}
