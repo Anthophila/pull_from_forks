0e9b3ebee61ceed3ba07152cbf52a8f6f31e0d66
Marco Gario <marcogario@github.com>
2020-06-26 14:43:21 +0200

    Update main4.c

--- main4.c
+++ main4.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
+       char *copy2;
+       copy2 = (char *)malloc(strlen(argv[0]));
+       strcpy(copy2, argv[0]);
+       return copy2;
        
-       return 0;
+       
 }
